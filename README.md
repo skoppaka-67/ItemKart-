0098cf14cd8199532dead8858d554550a30fbeaa
Sai_Kiran_Koppaka <65151649+skoppaka-67@users.noreply.github.com>
2021-01-24 18:41:22 +0530

    Freeze

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# ItemKart-
+# ItemKart
 Online E-commerce store 
+
+
+#security 
+
+Implemented token based authentication and authorization using JWT and HTTPBasicAuth 
+
+
+#home page
+
+home page will contains list of item tags
+
+url for homepage : http://localhost:5000/home
+
+Required no credentials 
+
+# category api
+
+Based on selection in home page items belonging to selected tag will returned with pagination and back and forth navigation urls 
+
+
+Required no credentials 
+
+#add to cart 
+
+Automatically redirects to login page for authentication or on JWT token  expiration  
+
+once after successful login user can add item to cart with item name
+
+url for add to cart : http://localhost:5000/add_to_cart
+
+#show cart
+
+displays items in the cart
+
+Url : http://localhost:5000/show_cart
+
+#delete cart
+
+Api will Allows only authorized users to delete from the cart
+
+url : http://localhost:5000/remove_cart
+
+#logout 
+
+logout the user by destroying session and redirects to login page
+
+url : http://localhost:5000/logout
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 ##############################################################
 ##############################################################
 ################## Item Kart #################################
@@ -16,11 +15,10 @@
 
 # You can start writing your code for the above mentioned functionalities from here
 
-from flask import Flask, request, jsonify, flash, session, redirect, url_for, render_template,abort
+from flask import Flask, request, jsonify, flash, session, redirect, url_for, render_template, abort
 from flask_restful import Resource, Api
 from flask_httpauth import HTTPBasicAuth
-import re, json,jwt,datetime,copy
-from urllib.parse import unquote
+import re, json, jwt, datetime, copy
 from functools import wraps
 import pymongo
 from pymongo import MongoClient
@@ -33,24 +31,30 @@ api = Api(app)
 auth = HTTPBasicAuth()
 app.secret_key = "8aacf358ee03b9e906455587c9538669"
 cart_list = []
-User_DATA = db.users.find({"name" : "John Doe"}, {"_id": 0})[0]
+User_DATA = db.users.find({"name": "John Doe"}, {"_id": 0})[0]
 
 
 def token_required(f):
+    """
+    function to verify the jwt tokens, will will be used a decorator to the functions
+
+    """
+
     @wraps(f)
-    def decorated(*args,**kwargs):
+    def decorated(*args, **kwargs):
         token = None
         if 'token' in session:
             token = session['token']
-        if not  token:
+        if not token:
             return render_template("login.html")
         try:
-            data = jwt.decode(token,app.config['SECRET_KEY'])
+            data = jwt.decode(token, app.config['SECRET_KEY'])
             # current_user =  db.user.find({"username":data['username']}, {"_id": 0})[0]
             current_user = data['username']
         except:
-            return  render_template("login.html")
-        return f(current_user,*args,**kwargs)
+            return render_template("login.html")
+        return f(current_user, *args, **kwargs)
+
     return decorated
 
 
@@ -68,45 +72,57 @@ def login():
 
             session['username'] = request.form['username']
             session['password'] = request.form['password']
-            token = jwt.encode({'username': 'John Doe', 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},
-                               app.config['SECRET_KEY'])
+
+            """creartion of Jwt tokens  and stored in session  below token makes user to login after every 5 minutes"""
+
+            token = jwt.encode(
+                {'username': 'John Doe', 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},
+                app.config['SECRET_KEY'])
+
             session['token'] = token.decode('UTF-8')
 
             return redirect(url_for('add_to_cart'))
     return render_template("login.html")
 
 
-@app.route('/home',methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
-    item_list = db.items.distinct("tags")
-    return render_template('category_list.html',li=item_list)
+    """route to display tags """
 
-@app.route('/category',methods=['GET'])
+    item_list = db.items.distinct("tags")
+    return render_template('category_list.html', li=item_list)
+
+
+@app.route('/category', methods=['GET'])
 def category():
+    """route to display  items of selected category along with pagination and url to move back and forth """
 
     data = []
     temp_json = {}
     category = str(request.args['category_list'])
     limit = int(request.args['limit'])
-    offset = int(request.args['offset']) #final record index
-    starting_id = db.items.find().sort('_id',pymongo.ASCENDING)
-    last_id = starting_id[offset]['_id' ]#final record _id
+    offset = int(request.args['offset'])  # final record index
+    starting_id = db.items.find().sort('_id', pymongo.ASCENDING)
+    last_id = starting_id[offset]['_id']  # final record _id
     # item_list = db.items.distinct("tags")
 
     # for item_tag in item_list[offset-limit:offset]:
-    cursy = db.items.find({'_id':{'$gt':last_id},"tags": category},{"name":1,"price":1,"description" : 1}).sort('_id',pymongo.ASCENDING).limit(limit)
+    cursy = db.items.find({'_id': {'$gt': last_id}, "tags": category}, {"name": 1, "price": 1, "description": 1}).sort(
+        '_id', pymongo.ASCENDING).limit(limit)
     # datavalue[item_tag] =[record for record in cursy]
-    for rec in  cursy:
+    for rec in cursy:
         temp_json[category] = rec
         data.append(copy.deepcopy(temp_json))
         temp_json.clear()
-    next_url  = '/category'+ '?category_list='+ str(category)+'&limit=' + str(limit) + '&offset=' + str(offset+limit)
-    prev_url  = '/category'+ '?category_list='+str(category) +'&limit=' + str(limit) + '&offset=' + str(offset-limit)
+    next_url = '/category' + '?category_list=' + str(category) + '&limit=' + str(limit) + '&offset=' + str(
+        offset + limit)
+    prev_url = '/category' + '?category_list=' + str(category) + '&limit=' + str(limit) + '&offset=' + str(
+        offset - limit)
 
-    return jsonify({'result':data,'prev_url':prev_url,'next_url':next_url})
+    return jsonify({'result': data, 'prev_url': prev_url, 'next_url': next_url})
 
 
-@app.route('/add_to_cart',methods=['GET','POST'])
+@app.route('/add_to_cart', methods=['GET', 'POST'])
 @token_required
 def add_to_cart(current_user):
     if request.method == 'GET' and len(session) > 0:
@@ -115,16 +131,15 @@ def add_to_cart(current_user):
             return render_template('add_cart.html')
     if request.method == 'POST' and len(session) > 0:
         if current_user != 'John Doe':
-            return jsonify({'message':'cannot perform that function!'})
+            return jsonify({'message': 'cannot perform that function!'})
 
         if session['username'] == User_DATA['name']:
             temp_var = request.get_data()
             temp_var = temp_var.decode('utf-8')
             # temp_list = temp_var.split("&")
-            item_name = temp_var.split('=')[-1].replace("+"," ")
+            item_name = temp_var.split('=')[-1].replace("+", " ")
 
-
-            cursy = json.dumps(db.items.find({"name":item_name},{'_id':0})[0])
+            cursy = json.dumps(db.items.find({"name": item_name}, {'_id': 0})[0])
             if cursy in cart_list:
                 flash("Item already exists")
                 return render_template('add_cart.html')
@@ -135,7 +150,8 @@ def add_to_cart(current_user):
         return render_template('add_cart.html')
     return render_template('add_cart.html')
 
-@app.route('/show_cart',methods=['GET'])
+
+@app.route('/show_cart', methods=['GET'])
 @token_required
 def show_cart(current_user):
     if request.method == 'GET' and len(session) > 0:
@@ -143,11 +159,11 @@ def show_cart(current_user):
         if session['username'] == User_DATA['name']:
             if current_user != 'John Doe':
                 return jsonify({'message': 'cannot perform that function!'})
-            return jsonify({"items":[x.replace('"',"").replace("'",'') for x in cart_list]})
+            return jsonify({"items": [x.replace('"', "").replace("'", '') for x in cart_list]})
         return render_template('login.html')
 
 
-@app.route('/remove_cart',methods=['GET','POST'])
+@app.route('/remove_cart', methods=['GET', 'POST'])
 @token_required
 def remove_cart(current_user):
     if request.method == 'GET' and len(session) > 0:
@@ -176,14 +192,12 @@ def remove_cart(current_user):
     return render_template('remove_cart.html')
 
 
-
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it is there
-    print(session)
     session.clear()
 
     return redirect(url_for('login'))
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
